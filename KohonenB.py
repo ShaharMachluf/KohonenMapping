2c3b3cecf935c3ca55d8015d70ef171482cfdb2e
GiladFisher <giladfisher98@gmail.com>
2023-06-27 14:44:19 +0300

    generating random data with hand image

--- KohonenB.py
+++ KohonenB.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from sompy import SOMFactory
 
 def getDataFromImage(path):
     img = cv2.imread(path)
@@ -22,4 +23,25 @@ data[:, 1] = 1 - data[:, 1]
 plt.scatter(data[:, 0], data[:, 1])
 plt.show()
 
+map_size = [20, 20]  # Size of the SOM grid
+som = SOMFactory.build(data, map_size, mask=None, mapshape='planar', lattice='rect')
+
+# Initialize a figure for visualization
+fig = plt.figure()
+
+# Train the SOM
+for i in range(1000):  # Perform 1000 iterations
+    som.train(1)  # Train for one iteration
+
+    # Get the trained SOM grid
+    som_grid = som._normalizer.denormalize_by(som.data_raw, som.codebook.matrix)
+
+    # Plot the SOM grid
+    plt.clf()
+    plt.imshow(som_grid, origin='lower')
+    plt.title(f'Iteration {i+1}')
+    plt.colorbar()
+    plt.pause(0.01)  # Pause for a short time to display the plot
+
+plt.show()  # Show the final plot
 
